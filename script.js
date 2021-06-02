70f221aba3b3332c8980ae483a19790e377641f6
HannahJT <>
2021-06-02 14:15:44 +0000

    got rid of trailing spaces.

--- script.js
+++ script.js
@@ -20,6 +20,6 @@ function show () {
   document.getElementById('text').style.display = 'block'
 }
 
-function background () {    
+function background () {
   document.body.style.backgroundColor = 'lightblue'
 }
